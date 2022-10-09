5bb333907c36812f5ed83fdba612b36623edffb9
dominikaglow <https://github.com/dominikaglow>
2022-10-09 16:26:06 +0200

    Initial commit

---
+++ zad10.py
@@ -0,0 +1,9 @@
+line = "Python to jezyk programowania wysokiego" \
+	      " poziomu ogolnego przeznaczenia"
+myList = line.split(',')
+i = 0  # ilosc slow
+print(myList)
+for z in myList:
+       print(z.split())
+       i = i + 1
+print(i)
