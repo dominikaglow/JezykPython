50fa6c5602addf2705233b6b3265a95f4e3c125e
dominikaglow <https://github.com/dominikaglow>
2022-10-09 21:35:32 +0200

    final version

--- zad14.py
+++ zad14.py
@@ -1,1 +1,15 @@
-#todozad
+line = "Python wspiera różne paradygmaty programowania mianowicie obiektowy " \
+       "imperatywny oraz w mniejszym stopniu funkcyjny"
+
+listWords = line.split()
+print(listWords)
+
+maxLen = 0
+maxWord = ""
+
+for words in listWords:
+    if(len(words) > maxLen):
+        maxLen = len(words)
+        maxWord = words
+
+print("Najdluzszy wyraz: " + maxWord + ", jego dlugosc: ", maxLen)
