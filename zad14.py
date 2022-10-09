bd0954dca819dc40cf2f75c5c2185e7fe7727d49
dominikaglow <https://github.com/dominikaglow>
2022-10-09 16:43:52 +0200

    final version

--- zad14.py
+++ zad14.py
@@ -1,9 +1,1 @@
-l = [12, 58, 4, 18, 286, 846]
-result = ""
-
-print("Lista l: ", l)
-
-for elem in l:
-    result = result + str(elem)
-
-print("Napis powstaly z elementow listy l: " + result)
+#todozad
