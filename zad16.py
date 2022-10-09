905719ef75b334fd010c56d43625d083cc0354b0
dominikaglow <https://github.com/dominikaglow>
2022-10-09 16:45:24 +0200

    final version

---
+++ zad16.py
@@ -0,0 +1,7 @@
+line = "Python to jezyk programowania wysokiego" \
+       " poziomu GvR ogolnego przeznaczenia"
+
+newLine = line.replace("GvR", "Guido van Rossum")
+
+print("Line przed zamiana: " + line)
+print("Line po zamianie: " + newLine)
