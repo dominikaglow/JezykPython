9b0a6c5453c323a8332cd88c15d1700a63bce6d5
dominikaglow <https://github.com/dominikaglow>
2022-10-10 12:01:18 +0200

    final version

---
+++ zad19.py
@@ -0,0 +1,12 @@
+L = [4, 85, 3, 984, 1, 537, 75, 37, 2, 45, 74, 78, 96, 8]
+
+text = ""
+
+for elements in L:
+    txtNum = str(elements)
+    if(len(txtNum) < 3):
+        txtNum = txtNum.zfill(3)
+    text += txtNum
+
+print("Lista L: ", L)
+print("Zbudowany napis: " + text)
